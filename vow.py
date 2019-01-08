ae886955cea5d6e9aca140a256dc258b59236441
jayasri <46441473+jayasrisambath@users.noreply.github.com>
2019-01-08 15:40:08 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,5 +1,9 @@
 l=int(input())
-if l in ('a','e','i','o','u'):
-         print("Vowel")
+if l>='a' and l<='z':
+         if l in ('a','e','i','o','u'):
+                  print("Vowel")
+         else:
+                  print("Consonant")
 else:
          print("Invalid")
+        
