53f6d0bf65a0ea1ac0059288254903b9f6861615
jayasri <46441473+jayasrisambath@users.noreply.github.com>
2019-01-09 16:11:56 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,10 +1,6 @@
 x=int(input())
 c=0
-if x>9 and x<-9:
-      print("1")
-else:
-      while(x>0):
-            r=x/10
-            c=c+1
-            x=r
-      print(c)
+while(x>0):
+      c=c+1
+      x=x//10
+print(c)
